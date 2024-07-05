72552272566a4fb424a2ed4849d06ad1aa2dee79
Roni Saha <roni.cse@gmail.com>
2024-07-05 15:40:42 +0600

    Add policy page

--- android/app/src/main/java/net/xiidea/easypass/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package net.xiidea.easypass;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Easy Password Generator',
-  webDir: 'www'
-};
-
-export default config;
